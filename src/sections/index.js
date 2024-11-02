82121d849db02c1e90234312934fb8fb0c9e7c7a
Charles <chazylites@gmail.com>
2024-11-02 14:14:58 +0100

    completed the navbar

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,2 +1,19 @@
-import Hero from './Footer';
+ import Hero from './Footer';
 import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}

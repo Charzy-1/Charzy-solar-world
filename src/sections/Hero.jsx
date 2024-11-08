411811245ee0e6f1062746aa912619b9aa8c4941
Charles <chazylites@gmail.com>
2024-11-08 17:31:42 +0100

    completed hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,9 +15,9 @@ const Hero = () => {
       className="hero-section w-full flex xl:flex-row flex-col justify-center items-center min-h-screen gap-10 max-container"
     >
       {/* Text Content */}
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:px-[3rem] xl:px-[3.5rem] pt-28">
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-4'>
         <p className="text-xl font-montserrat text-coral-red">Power Your Future with</p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+        <h1 className="mt-5 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="relative z-10 pr-10">Reliable</span> <br />
           <span className="text-coral-red">Solar Solution</span> 
         </h1>

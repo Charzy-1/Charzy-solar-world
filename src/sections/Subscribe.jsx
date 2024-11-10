2eb1f1dbff24878b49c4269006cd5f19a082966a
Charles <chazylites@gmail.com>
2024-11-10 20:27:14 +0100

    started the subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,6 +1,19 @@
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section id="contact-us"
+    className="max-container flex justify-between items-center max-lg:flex-col gap-10">
+      <h3 className="font-bold leading-[68px] lg:max-w-md font-palanquin text-4xl">
+        Sign up for {''}
+        <span className="text-coral-red">Updates</span>  & Newsletter
+      </h3>
+      <div>
+        <input type='text'
+        placeholder="charles.adikankwu@exultedeagles.com" className="input" />
+        <div>
+          <button label='Sign Up' />
+        </div>
+      </div>
+    </section>
   )
 }
 

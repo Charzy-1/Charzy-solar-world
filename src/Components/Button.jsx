5e9ddd7a187b029cd1e9f1fb12e8170f8161fe4e
Charles <chazylites@gmail.com>
2024-11-11 10:22:30 +0100

    completed the subcribe section

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,10 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, fullWidth }) => {
   return (
     <button className='flex justify-center items-center gap-2 px-5 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white'>
         {label}
 
         {iconURL && <img 
-        src={iconURL} alt="Rrow right icon" className="ml-2 rounded-full w-5 h-5"
+        src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"
         />}
     </button>
   )
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,16 +1,21 @@
+import Button from "../Components/Button"
+
 const Subscribe = () => {
   return (
     <section id="contact-us"
     className="max-container flex justify-between items-center max-lg:flex-col gap-10">
-      <h3 className="font-bold leading-[68px] lg:max-w-md font-palanquin text-4xl">
-        Sign up for {''}
-        <span className="text-coral-red">Updates</span>  & Newsletter
+      <h3 className="font-bold text-center leading-[68px] lg:max-w-md font-palanquin text-4xl">
+        Sign up for <span className="text-coral-red">Updates</span> & Newsletter
       </h3>
-      <div>
+
+      {/* imput and button container */}
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type='text'
-        placeholder="charles.adikankwu@exultedeagles.com" className="input" />
-        <div>
-          <button label='Sign Up' />
+        placeholder="subsribeu@exultedeagles.com" className="input" />
+
+        {/* button container */}
+        <div className="flex max-sm:justify-end items-center mas-sm:w-full"> 
+          <Button label='Sign Up' fullWidth />
         </div>
       </div>
     </section>
